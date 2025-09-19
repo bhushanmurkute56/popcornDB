2fc3d573e386cd8018dc7e3b3e84802c3e35520c
bhushanmurkute56 <bhushanmurkute56@gmail.com>
2025-09-19 16:32:48 +0530

    mongodb integrated

--- Server/index.js
+++ Server/index.js
@@ -2,15 +2,26 @@ import express from 'express';
 import cors from "cors";
 import dotenv from 'dotenv';
 dotenv.config();
+import mongoose from 'mongoose';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+const connectDB = async () => {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+
+    if(conn){
+        console.log("\nMongoDB Connected ✨");
+    }
+}
+
 app.get("/health", (req, res) => {
     res.json({ status : "OK", message : "Server is healthy"});
 })
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 
-app.listen(PORT, () => {console.log(`Server is running on port : ${PORT}`)});
+app.listen(PORT, () => {console.log(`Server is running on port : ${PORT} ✅`)
+connectDB()
+})
